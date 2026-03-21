5c04e613a09d57aa79fbf8c92fea1a70768b62f6
cyd <github@simon-schindler.de>
2026-03-21 16:55:07 +0100

    adding missing dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	// Source: https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
 	implementation("org.xerial:sqlite-jdbc:3.51.3.0")
+	// Source: https://mvnrepository.com/artifact/org.hibernate.orm/hibernate-community-dialects
+	implementation("org.hibernate.orm:hibernate-community-dialects:7.3.0.Final")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
