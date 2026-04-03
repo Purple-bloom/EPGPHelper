c9d03686414f1d30121da4bb965a19b701ac6f50
cyd <github@simon-schindler.de>
2026-04-03 18:32:56 +0200

    need empty characterDto constructor

--- src/main/java/custom/cyd/epgphelperbackend/Dto/CharacterDto.java
+++ src/main/java/custom/cyd/epgphelperbackend/Dto/CharacterDto.java
@@ -19,6 +19,8 @@ public class CharacterDto implements Serializable {
         this.characterClass = character.getCharacterClass();
     }
 
+    public CharacterDto(){}
+
     public Long getId() {
         return id;
     }
